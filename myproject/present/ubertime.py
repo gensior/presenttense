60809670f3cb25666a979b45aac0803f3a68a3e5
Jesse Franceschini <j.franceschini@gmail.com>
2011-06-19 23:37:38 -0700

    Added basic date/time calculations.

---
+++ myproject/present/ubertime.py
@@ -0,0 +1,27 @@
+from mx.DateTime import DateTime, DateTimeDeltaFromSeconds, now
+
+present = now()
+
+def historical(historyend=DateTime(present.year, 12, 31, 23, 59, 59)):
+	historystart = DateTime(-10000, 1 ,1)
+	return (historyend - historystart).seconds
+
+def yearical(yearend=DateTime(present.year, 12, 31, 23, 59, 59)):
+	yearstart = DateTime(present.year, 1, 1)
+	return (yearend - yearstart).seconds
+
+def yearify(historydate):
+	historyscale = historical()
+	historypoint = historical(historyend=historydate)
+	historyratio = (historyscale - historypoint)/historyscale
+	yearscale = yearical()
+	yearpoint = DateTimeDeltaFromSeconds(yearscale * historyratio)
+	return DateTime(present.year, 12, 31, 23, 59, 59) - yearpoint
+
+def historify(yeardate):
+	yearscale = yearical()
+	yearpoint = yearical(yearend=yeardate)
+	yearratio = (yearscale - yearpoint)/yearscale
+	historyscale = historical()
+	historypoint = DateTimeDeltaFromSeconds(historyscale * yearratio)
+	return DateTime(present.year, 12, 31, 23, 59, 59) - historypoint
